8f49007ae9162a4562273d99461c2a6d7b1ca581
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-03-14 12:39:19 -0400

    Converted Ih and O to primitives and made some adjustments to positioning to make it work

--- tracelet4/tracelet_substraction.js
+++ tracelet4/tracelet_substraction.js
@@ -43,8 +43,8 @@ function main(args) {
 
 		// move cut-out to appropriate location
 		cutOut.setTransform(new Translation(
-			section_size * design[i][0] - curve_length / 2 + bar_width / 2,
-			section_size * design[i][1] - height / 2 + bar_width / 2,
+			section_size * design[i][0] - curve_length / 2 + bar_width,
+			section_size * design[i][1] - height / 2 + bar_width,
 			0));
 
 		bracelet = new Subtraction(bracelet, cutOut);
@@ -76,8 +76,8 @@ function generateTetramino(squares) {
 
 	for (var i = 0; i < squares.length; i++) {
 		var square = new Box(
-			(squares[i][0] + 0.5) * section_size,
-			(squares[i][1] + 0.5) * section_size,
+			(squares[i][0] + 0.5) * section_size - bar_width / 2,
+			(squares[i][1] + 0.5) * section_size - bar_width / 2,
 			0,
 			square_side,
 			square_side,
@@ -91,8 +91,8 @@ function generateTetramino(squares) {
 			if (squares[i][0] == squares[j][0] && squares[i][1] - squares[j][1] == 1) {
 				// TODO draw line under
 				tetramino.add(new Box(
-					(squares[i][0] + 0.5) * section_size,
-					(squares[i][1] + 0.5) * section_size - square_side + bar_width / 2, // one below
+					(squares[i][0] + 0.5) * section_size - bar_width / 2,
+					(squares[i][1] + 0.5) * section_size - bar_width / 2 - square_side + bar_width / 2, // one below
 					0,
 					square_side,
 					bar_width,
@@ -104,8 +104,8 @@ function generateTetramino(squares) {
 			if (squares[i][1] == squares[j][1] && squares[i][0] - squares[j][0] == 1) {
 				// TODO draw line on the left
 				tetramino.add(new Box(
-					(squares[i][0] + 0.5) * section_size - square_side + bar_width / 2,  // one on the left
-					(squares[i][1] + 0.5) * section_size,
+					(squares[i][0] + 0.5) * section_size - bar_width / 2 - square_side + bar_width / 2,  // one on the left
+					(squares[i][1] + 0.5) * section_size - bar_width / 2,
 					0,
 					bar_width,
 					square_side,
@@ -140,10 +140,13 @@ function init() {
 	voxelSize = 0.4*MM; // point precision
 
 	// tetraminos are a combination of horizontal and vertical strokes on borders of 4x4 grid surrounding 4 of 16 cells
-	Ih = generateTetramino([[0, 0], [1, 0], [2, 0], [3, 0]]); // I horizontal (4-bar)
+//	Ih = generateTetramino([[0, 0], [1, 0], [2, 0], [3, 0]]); // I horizontal (4-bar)
+	Ih = new Box(	(4 * section_size - bar_width) / 2,	square_side / 2,	0,
+					4 * section_size - bar_width,		square_side,		thickness * 2); // I horizontal (4-bar)
 	Iv = generateTetramino([[0, 0], [0, 1], [0, 2], [0, 3]]); // I vertical (4-bar)
 
-	O  = generateTetramino([[0, 0], [0, 1], [1, 0], [1, 1]]); // O (square)
+	O  = new Box(	(2 * section_size - bar_width) / 2,	(2 * section_size - bar_width) / 2,	0,
+					(2 * section_size - bar_width),		2 * section_size - bar_width,		thickness * 2); // O (square)
 
 	S  = generateTetramino([[0, 1], [1, 1], [1, 0], [2, 0]]); // S
 	Sv = generateTetramino([[0, 0], [0, 1], [1, 1], [1, 2]]); // S vertical
